bc3df224aef311769d788cbb5c968b0d3480023b
SoumyadeepSpacejoy <soumyadeep.saharoy@spacejoy.com>
2024-10-20 10:37:41 +0530

    server health route added

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 const user = require('./user');
 
 module.exports = (app) => {
+    app.get(`/`, (req, res) => {
+        return res.status(200).json({ health: 'ok' });
+    });
+
     app.use('/v1/user', user);
     console.log('route added... ✅');
 };
